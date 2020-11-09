5540366431bbfd0a1083242dee0b2feb98e35a39
bryan-teng <teng.weiyan.bryan@gmail.com>
2020-11-09 18:44:07 +0000

    Initial commit

---
+++ src/Meeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Meeting() {
+  return (
+    <div className="Meeting">
+      <h1>Meeting</h1>
+    </div>
+  );
+}
+
+export default Meeting;
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="NavbarTitle"><h3>Navbar Testing</h3></div>
+    </div>
+  );
+}
+
+export default Navbar;
